43b1fdff55d56ce71a5060b0d3f28a8354c0326a
Fahad Rehman <wolfboy0786@gmail.com>
2023-06-24 15:28:53 +0500

    grouping done

--- turing3/src/MainCallsPage/MainCallsPage.js
+++ turing3/src/MainCallsPage/MainCallsPage.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-
 import axios from 'axios';
 
 const MainCallsPage = () => {
   const [data, setData] = useState(null);
   const [token, setToken] = useState();
   const [archivedCalls, setArchivedCalls] = useState([]);
+  const [grouped, setGrouped] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +26,6 @@ const MainCallsPage = () => {
 
         const responseData = meResponse.data;
         setData(responseData.nodes);
-        console.log(responseData.nodes);
       } catch (error) {
         console.log(error);
       }
@@ -52,6 +51,10 @@ const MainCallsPage = () => {
     setArchivedCalls((prevArchivedCalls) => [...prevArchivedCalls, call]);
   };
 
+  const toggleGrouped = () => {
+    setGrouped((prevGrouped) => !prevGrouped);
+  };
+
   if (!data) {
     return <div>Loading...</div>;
   }
@@ -61,18 +64,42 @@ const MainCallsPage = () => {
     return <div>Error: Invalid data structure</div>;
   }
 
+  // Group calls by date
+  const groupedCalls = {};
+  if (grouped) {
+    data.forEach((call) => {
+      const date = new Date(call.created_at).toLocaleDateString();
+      if (!groupedCalls[date]) {
+        groupedCalls[date] = [];
+      }
+      groupedCalls[date].push(call);
+    });
+  } else {
+    groupedCalls['ungrouped'] = data;
+  }
+
   return (
     <div>
-      {data.map((call) => (
-        <div key={call.id}>
-          <Link to={`/calldetailpage/${call.id}/${token}`}>
-            <div>
-              {call.from} - {call.to} - this is id {call.id}
+      <button onClick={toggleGrouped}>
+        {grouped ? 'Ungroup Calls' : 'Group Calls'}
+      </button>
+
+      {Object.entries(groupedCalls).map(([date, calls]) => (
+        <div key={date}>
+          {grouped && <h3>{date}</h3>}
+          {calls.map((call) => (
+            <div key={call.id}>
+              <Link to={`/calldetailpage/${call.id}/${token}`}>
+                <div>
+                  {call.from} - {call.to} - this is id {call.id}
+                </div>
+              </Link>
+              <button onClick={() => handleArchiveCall(call)}>Archive</button>
             </div>
-          </Link>
-          <button onClick={() => handleArchiveCall(call)}>Archive</button>
+          ))}
         </div>
       ))}
+
       <h2>Archived Calls:</h2>
       {archivedCalls.map((call) => (
         <div key={call.id}>
