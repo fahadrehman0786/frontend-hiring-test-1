f39c912451c0649ec89e2e016ec0c093d4222567
Fahad Rehman <wolfboy0786@gmail.com>
2023-06-24 22:37:45 +0500

    pagination done

--- turing3/src/CallDetailPage/CallDetailPage.js
+++ turing3/src/CallDetailPage/CallDetailPage.js
@@ -1,19 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import Paper from '@mui/material/Paper'; // Add this line
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const CallDetailPage = () => {
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
-  const { id, token } = useParams();
+export default function CallDetailPage({open, setOpen, id, token}) {
+
   const [callData, setCallData] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
 
-
+        console.log(token+ "this is token");
 
         const meResponse = await axios.get(`https://frontend-test-api.aircall.io/calls/${id}`, {
             headers: {
@@ -23,6 +48,7 @@ const CallDetailPage = () => {
 
           setCallData(meResponse.data)
           setIsLoading(false);
+          console.log(meResponse.data)
 
       } catch (error) {
         console.log(error);
@@ -32,23 +58,60 @@ const CallDetailPage = () => {
     fetchData();
   }, []);
 
-  
+
   return (
     <div>
-      <h2>Call Detail</h2>
-      <p>Call ID: {id}</p>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <p>from: {callData.from}</p>
-          <p>to: {callData.to}</p>
-          {/* Additional code for displaying call details */}
-        </>
-      )}
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open full-screen dialog
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar component={Paper} position="relative"> {/* Use component prop to specify Paper */}
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        {isLoading || !token || !id ? (
+          <div>Loading...</div>
+        ) : (
+          <List>
+            <ListItem>
+              <ListItemText primary="From" secondary={callData.from} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="To" secondary={callData.to} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Duration" secondary={callData.duration} />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Archived Status"
+                secondary={callData.is_archived ? "Archived" : "Not Archived"}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Via" secondary={callData.via} />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Direction" secondary={callData.direction} />
+            </ListItem>
+          </List>
+        )}
+      </Dialog>
     </div>
-  );
   
-};
-
-export default CallDetailPage;
+  
+  );
+}
--- turing3/src/MainCallsPage/MainCallsPage.js
+++ turing3/src/MainCallsPage/MainCallsPage.js
@@ -1,31 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Badge,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+} from "@mui/material";
+
+import CallDetailPage from "../CallDetailPage/CallDetailPage";
 
 const MainCallsPage = () => {
   const [data, setData] = useState(null);
   const [token, setToken] = useState();
   const [archivedCalls, setArchivedCalls] = useState([]);
   const [grouped, setGrouped] = useState(false);
+  const [notes, setNotes] = useState({});
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [noteContent, setNoteContent] = useState("");
+  const [currentCallId, setCurrentCallId] = useState(null);
+  const [openDetail, setOpenDetail] = useState(false);
+  const [currentCallIdForDetail, setCurrentCallIdForDetail] = useState(null);
+  const [currentToken, setCurrentToken] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const loginResponse = await axios.post(
-          'https://frontend-test-api.aircall.io/auth/login',
-          { username: 'fahad', password: 'test123' }
+          "https://frontend-test-api.aircall.io/auth/login",
+          { username: "fahad", password: "test123" }
         );
 
         setToken(loginResponse.data.access_token);
 
-        const meResponse = await axios.get('https://frontend-test-api.aircall.io/calls', {
-          headers: {
-            Authorization: `Bearer ${loginResponse.data.access_token}`,
-          },
-        });
+        const meResponse = await axios.get(
+          "https://frontend-test-api.aircall.io/calls",
+          {
+            headers: {
+              Authorization: `Bearer ${loginResponse.data.access_token}`,
+            },
+          }
+        );
 
         const responseData = meResponse.data;
         setData(responseData.nodes);
+        console.log(responseData.nodes);
       } catch (error) {
         console.log(error);
       }
@@ -35,18 +66,20 @@ const MainCallsPage = () => {
   }, []);
 
   const handleCallItemClick = async (call) => {
-    console.log('This is tok ' + token);
+    console.log("This is tok " + token);
 
-    console.log('Clicked call:', call);
-    const meResponse = await axios.get(`https://frontend-test-api.aircall.io/calls/${call.id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    console.log("Clicked call:", call);
+    const meResponse = await axios.get(
+      `https://frontend-test-api.aircall.io/calls/${call.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
     console.log(meResponse);
   };
-
 
   const handleArchiveCall = async (call) => {
     try {
@@ -59,27 +92,75 @@ const MainCallsPage = () => {
           },
         }
       );
-  
+
       // Update the archived status of the call
       const updatedCall = { ...call, is_archived: true };
-  
+
       // Find the index of the call in the data array
       const callIndex = data.findIndex((c) => c.id === call.id);
-  
+
       // Create a new data array with the updated call
       const updatedData = [...data];
       updatedData[callIndex] = updatedCall;
-  
+
       // Update the state variables
       setData(updatedData);
-      setArchivedCalls((prevArchivedCalls) => [...prevArchivedCalls, updatedCall]);
-  
-      console.log('Archive call response:', response.data);
+      setArchivedCalls((prevArchivedCalls) => [
+        ...prevArchivedCalls,
+        updatedCall,
+      ]);
+
+      console.log("Archive call response:", response.data);
     } catch (error) {
-      console.error('Error archiving call:', error.message);
+      console.error("Error archiving call:", error.message);
     }
   };
-  
+
+  const handleAddNote = (callId) => {
+    setCurrentCallId(callId);
+    setNoteContent("");
+    setNotes((prevNotes) => ({
+      ...prevNotes,
+      [callId]: {},
+    }));
+    setDialogOpen(true);
+  };
+
+  const handleNoteChange = (event) => {
+    setNoteContent(event.target.value);
+  };
+
+  const handleSubmitNote = async () => {
+    const callId = currentCallId;
+    try {
+      const response = await axios.post(
+        `https://frontend-test-api.aircall.io/calls/${callId}/note`,
+        {
+          content: noteContent,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Clear the note textarea and hide it
+      setNotes((prevNotes) => ({
+        ...prevNotes,
+        [callId]: {
+          showTextArea: false,
+          content: "",
+        },
+      }));
+
+      setDialogOpen(false);
+
+      console.log("Submit note response:", response.data);
+    } catch (error) {
+      console.error("Error submitting note:", error.message);
+    }
+  };
 
   const toggleGrouped = () => {
     setGrouped((prevGrouped) => !prevGrouped);
@@ -90,7 +171,7 @@ const MainCallsPage = () => {
   }
 
   if (!Array.isArray(data)) {
-    console.log('Unexpected data structure:', data);
+    console.log("Unexpected data structure:", data);
     return <div>Error: Invalid data structure</div>;
   }
 
@@ -105,46 +186,153 @@ const MainCallsPage = () => {
       groupedCalls[date].push(call);
     });
   } else {
-    groupedCalls['ungrouped'] = data;
+    groupedCalls["ungrouped"] = data;
   }
+  
+  const handleDetailOpen = (callid, token) => {
+    Promise.resolve()
+      .then(() => {
+        setCurrentCallIdForDetail(callid);
+        setCurrentToken(token);
+      })
+      .then(() => {
+        console.log(callid);
+        console.log(token);
+        console.log(currentToken + " this is currentToken");
+        console.log(currentCallIdForDetail + " this is currentID");
+        setOpenDetail(true);
+      });
+  };
+// Pagination state
+
+
+const totalPages = Math.ceil(data.length / itemsPerPage);
+
+// Calculate current page data
+const indexOfLastItem = currentPage * itemsPerPage;
+const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+// Change page
+const handlePageChange = (pageNumber) => {
+  setCurrentPage(pageNumber);
+};
 
   return (
     <div>
-      <button onClick={toggleGrouped}>
-        {grouped ? 'Ungroup Calls' : 'Group Calls'}
-      </button>
+      <Button variant="contained" onClick={toggleGrouped}>
+        {grouped ? "Ungroup Calls" : "Group Calls"}
+      </Button>
   
       {Object.entries(groupedCalls).map(([date, calls]) => (
         <div key={date}>
           {grouped && <h3>{date}</h3>}
-          {calls.map((call) => (
-            <div key={call.id}>
-              <Link to={`/calldetailpage/${call.id}/${token}`}>
-                <div>
-                  {call.from} - {call.to} - this is id {call.id}
-                </div>
-              </Link>
-              <button onClick={() => handleArchiveCall(call)}>Archive</button>
-              <div>
-                {call.is_archived ? 'Archived' : 'Not Archived'}
-              </div>
-            </div>
-          ))}
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>From</TableCell>
+                  <TableCell>To</TableCell>
+                  <TableCell>Direction</TableCell>
+                  <TableCell>Duration</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Add Note</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {currentItems.map((call) => (
+                  <TableRow key={call.id} onClick={() => { handleDetailOpen(call.id, token) }}>
+                    <TableCell>{call.from}</TableCell>
+                    <TableCell>{call.to}</TableCell>
+                    <TableCell>{call.direction}</TableCell>
+                    <TableCell>{call.duration}</TableCell>
+                    <TableCell>
+                      {call.is_archived ? (
+                        <span
+                          style={{
+                            color: "gray",
+                            cursor: "pointer",
+                            textDecoration: "none",
+                          }}
+                          onClick={() => handleArchiveCall(call)}
+                        >
+                          Archived
+                        </span>
+                      ) : (
+                        <Button
+                          style={{
+                            color: "white",
+                            cursor: "pointer",
+                            textDecoration: "none",
+                          }}
+                          variant="contained"
+                          onClick={() => handleArchiveCall(call)}
+                        >
+                          Not Archived
+                        </Button>
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        onClick={() => handleAddNote(call.id)}
+                      >
+                        Add Note
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       ))}
   
-      <h2>Archived Calls:</h2>
-      {archivedCalls.map((call) => (
-        <div key={call.id}>
-          {call.from} - {call.to} - this is id {call.id}
-          <div>
-            Archived
-          </div>
-        </div>
-      ))}
+      {/* Pagination */}
+      <div>
+        {totalPages > 1 &&
+          Array.from({ length: totalPages }, (_, index) => (
+            <Button
+              key={index + 1}
+              variant="contained"
+              onClick={() => handlePageChange(index + 1)}
+              disabled={currentPage === index + 1}
+            >
+              {index + 1}
+            </Button>
+          ))}
+      </div>
+  
+      {/* Note Dialog */}
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <DialogTitle>Add Note</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Note"
+            type="text"
+            fullWidth
+            value={noteContent}
+            onChange={handleNoteChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogOpen(false)}>Cancel</Button>
+          <Button onClick={handleSubmitNote}>Add</Button>
+        </DialogActions>
+      </Dialog>
+  
+      {/* Call Detail Dialog */}
+      {openDetail && (
+        <CallDetailPage
+          callid={currentCallIdForDetail}
+          token={currentToken}
+          handleClose={() => setOpenDetail(false)}
+        />
+      )}
     </div>
   );
 };
 
 export default MainCallsPage;
-
